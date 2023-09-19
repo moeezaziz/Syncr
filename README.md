5a78707f889846ce132e61eba3488722ae6fe5a6
moeezaziz <moeezazizch@gmail.com>
2023-09-19 23:02:49 +0200

    implement unit tests

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# Syncr
-Python script for synchronisation of two directories
+# Folder Synchronization Tool
+
+## Overview
+
+This is a Python-based command-line tool for synchronizing two folders: a source folder and a replica folder. The tool ensures that the replica folder maintains an identical copy of the source folder. Synchronization is performed periodically, and all file creation, copying, and removal operations are logged to both a log file and the console output.
+
+## Features
+
+- **One-way synchronization:** Syncs content from the source folder to the replica folder.
+- **Periodic synchronization:** Configurable time interval for synchronization.
+- **Logging:** Detailed logging of synchronization operations.
+- **Command-line interface:** Easy-to-use command-line arguments for configuration.
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.6 or higher
+
+### Installation
+
+1. **Clone this repository** to your local machine:
+`git@github.com:moeezaziz/Syncr.git`
+
+2. **Navigate to the project directory**:
+`cd syncr`
+
+3. **Install the required dependencies** (if not already installed):
+`pip install -r requirements.txt`
+
+## Usage
+
+### Command-Line Arguments
+
+- `--source`: The source folder to sync from.
+- `--destination`: The replica folder to sync to.
+- `--log`: The log file to record synchronization operations.
+- `--interval`: The synchronization interval in seconds.
+
+### Example Usage
+`python sync_directories.py --source /path/to/source/folder --destination /path/to/replica/folder --log sync.log --interval 3600`
+
+This example synchronizes the source folder to the replica folder every 3600 seconds (1 hour) and logs operations to `sync.log`.
+
+You can also directly run `python sync_directories.py`and enter the arguments using the command line interface.
+### Running Tests
+
+You can run the included unit tests to ensure the tool's functionality:
+`test_sync_directories.py`
+
+## Contributing
+
+Contributions are welcome! If you'd like to contribute to this project, please follow these steps:
+
+1. **Fork the repository**.
+2. Create a new branch for your feature or bug fix.
+3. **Make your changes** and commit them with clear messages.
+4. **Push your changes** to your fork.
+5. **Submit a pull request** to the main repository.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+This tool was developed as a solution to a synchronization task and makes use of Python libraries such as `os`, `time`, `filecmp`, and `shutil`. Additionally, it uses the `click` library for command-line argument parsing.
+
+## Contact
+
+If you have any questions or suggestions, feel free to contact me at moeezazizch@gmail.com
--- sync_directories.py
+++ sync_directories.py
@@ -1,59 +1,99 @@
+#!/usr/bin/env python3
+
 import os
-import sys
 import time
 import filecmp
 import shutil
 import click
+from pathlib import Path
 
 
 class FolderSynchronizer:
-    def __init__(self, source, destination, log, interval):
-        self.source_dir = source
-        self.replica_dir = destination
-        self.log_file = log
+    def __init__(self, source: Path, destination: Path, log: Path, interval: int) -> None:
+        """Initializes the FolderSynchronizer with source and destination directories,
+        and a log file.
+
+        :param source: The source directory to sync from.
+        :param destination: The replica directory to sync to.
+        :param log: The log file to record synchronization operations.
+        :param interval: The time interval between each sync in seconds.
+        """
+        self.source_dir = Path(source)
+        self.destination_dir = Path(destination)
+        self.log_file = Path(log)
         self.interval = interval
 
-    def synchronize(self):
+    def synchronize(self) -> None:
+        """
+        Synchronizes the source directory with the destination directory.
+        """
         try:
             click.echo("---------Comparing source and destination-----------")
-            dcmp = filecmp.dircmp(self.source_dir, self.replica_dir)
-            if not dcmp.diff_files:
-                click.echo("No changes detected!")
+            if not os.path.isdir(self.destination_dir):
+                click.echo("Directory does not exist! Creating a new one...")
+                os.mkdir(self.destination_dir)
+                self._log(f"Directory created at {self.destination_dir}")
+            dcmp = filecmp.dircmp(self.source_dir, self.destination_dir)
+            click.echo(dcmp.report())
             self._copy_new_files(dcmp.left_only)
             self._remove_extra_files(dcmp.right_only)
             self._update_changed_files(dcmp.diff_files)
         except Exception as e:
             self._log(f"Error: {str(e)}")
+        click.echo("--------------Comparison complete----------------------")
 
-    def _copy_new_files(self, files):
+    def _copy_new_files(self, files: list) -> None:
+        """
+        Copies new files or folders from the source to the destination directory.
+
+        :param files: List of new files or folders.
+        """
         for name in files:
-            src_path = os.path.join(self.source_dir, name)
-            dst_path = os.path.join(self.replica_dir, name)
-            if os.path.isfile(src_path):
+            src_path = self.source_dir / name
+            dst_path = self.destination_dir / name
+            if not dst_path.exists():
+                if src_path.is_file():
+                    shutil.copy2(src_path, dst_path)
+                    self._log(f"File copied: {src_path} -> {dst_path}")
+                else:
+                    shutil.copytree(src_path, dst_path, False, None)
+                    self._log(f"Folder copied: {src_path} -> {dst_path}")
+
+    def _remove_extra_files(self, files: list) -> None:
+        """
+        Removes extra files or folders from the destination directory.
+
+        :param files: List of extra files or folders.
+        """
+        for name in files:
+            path = self.destination_dir / name
+            if path.exists():
+                if path.is_file():
+                    path.unlink()
+                    self._log(f"File removed: {path}")
+                else:
+                    shutil.rmtree(path)
+                    self._log(f"Folder removed: {path}")
+
+    def _update_changed_files(self, files: list) -> None:
+        """
+        Updates changed files in the destination directory with source versions.
+
+        :param files: List of changed files.
+        """
+        for name in files:
+            src_path = self.source_dir / name
+            dst_path = self.destination_dir / name
+            if dst_path.exists():
                 shutil.copy2(src_path, dst_path)
-                self._log(f"File copied: {src_path} -> {dst_path}")
-            else:
-                shutil.copytree(src_path, dst_path, False, None)
-                self._log(f"Folder copied: {src_path} -> {dst_path}")
+                self._log(f"File updated: {src_path} -> {dst_path}")
 
-    def _remove_extra_files(self, files):
-        for name in files:
-            path = os.path.join(self.replica_dir, name)
-            if os.path.isfile(path):
-                os.remove(path)
-                self._log(f"File removed: {path}")
-            else:
-                shutil.rmtree(path)
-                self._log(f"Folder removed: {path}")
+    def _log(self, message: str) -> None:
+        """
+        Logs a message to the specified log file and prints it to the console.
 
-    def _update_changed_files(self, files):
-        for name in files:
-            src_path = os.path.join(self.source_dir, name)
-            dst_path = os.path.join(self.replica_dir, name)
-            shutil.copy2(src_path, dst_path)
-            self._log(f"File updated: {src_path} -> {dst_path}")
-
-    def _log(self, message):
+        :param message: The message to log.
+        """
         with open(self.log_file, "a") as log:
             timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
             log.write(f"[{timestamp}] {message}\n")
@@ -61,15 +101,19 @@ class FolderSynchronizer:
 
 
 @click.command()
-@click.option('--source', required=True, prompt='Source folder')
-@click.option('--destination', required=True, prompt='Destination folder')
-@click.option('--log', required=True, prompt='Log file')
-@click.option('--interval', required=True, prompt='Sync interval', type=int)
-def main(source, destination, log, interval):
+@click.option('--source', required=True, type=click.Path(exists=True, file_okay=False, dir_okay=True),
+              prompt='Source folder', help='Source folder')
+@click.option('--destination', required=True, type=click.Path(file_okay=False, dir_okay=True),
+              prompt='Destination folder', help='Destination folder')
+@click.option('--log', required=True, type=click.Path(writable=True),
+              prompt='Log file', help='Log file')
+@click.option('--interval', required=True, type=int, prompt='Sync interval in seconds',
+              help='Sync interval in seconds')
+def main(source: Path, destination: Path, log: Path, interval: int) -> None:
     synchronizer = FolderSynchronizer(source, destination, log, interval)
     while True:
         synchronizer.synchronize()
-        time.sleep(synchronizer.interval)  # Synchronize every hour (3600 seconds)
+        time.sleep(synchronizer.interval)
 
 
 if __name__ == "__main__":
